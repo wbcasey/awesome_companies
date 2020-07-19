869a826b6e2be58ec1f79b5d8cda48d4c6704a78
Casey Provost <casey@workbright.com>
2020-07-19 12:35:04 -0400

    tailwind-ifying views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    sessions: 'user/sessions',
+    registrations: 'user/registrations',
+    passwords: "user/passwords",
+    unlocks: "user/unlocks",
+    confirmations: "user/confirmations"
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_scope :user do
